ab93bc147b68758d54a5f4f123b40a350eb93579
RaminHels <r.finland88@gmail.com>
2021-07-17 18:14:10 +0430

    init

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var usersRouter = require("./routes/users");
-var app = express();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+// const usersRouter = require("./routes/users");
 const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
+const app = express();
 Sentry.init({
-  dsn: "https://724e38e629e0438dabc2705babdd19dc@sentry.rayvarz.dev/4",
-  tracesSampleRate: 1.0
+  dsn: "https://fefc3318efb54a7d80be1d8374919dec@sentry.rayvarz.dev/3",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({
+      app
+    })
+  ],
+  tracesSampleRate: 1.0,
+  release: 1
 });
-
-app.get("/Exception", function mainHandler(req, res) {
-  throw new Error("An exception occured!");
-});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
 
-app.use("/users", usersRouter);
+app.use("/POW", function (req, res) {
+  res.send(Math.pow(req.query.a, req.query.b).toString());
+});
+
 app.get("/Hello", function rootHandler(req, res) {
   res.end("Hello world!!");
 });
-
-app.use("/POW", function (req, res, next) {
-  res.send(Math.pow(req.query.a, req.query.b).toString());
+// app.use("/users", usersRouter);
+app.get("/Exception", function mainHandler(req, res) {
+  throw new Error("An exception occured!");
 });
+
+app.use(Sentry.Handlers.errorHandler());
 var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
 });
--- public/Init.js
+++ public/Init.js
@@ -1,8 +1,17 @@
-﻿﻿// import * as Sentry from "@sentry/node";
 Sentry.init({
   environment: "Testing",
-  dsn: "https://724e38e629e0438dabc2705babdd19dc@sentry.rayvarz.dev/4",
+  dsn: "https://fefc3318efb54a7d80be1d8374919dec@sentry.rayvarz.dev/3",
   tracesSampleRate: 1.0,
   integrations: [new Sentry.Integrations.BrowserTracing()],
-  sampleRate: 1.0
+  // sampleRate: 1.0
+  release: 1
 });
+
+function CallPow(num1, num2) {
+  axios.get("/POW?a=" + num1 + "&b=" + num2).then((result) => {
+    let resultInput = document.getElementById("Result");
+    resultInput.value = result.data;
+  });
+}
+
+CallPow(2, 3);
